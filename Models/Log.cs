c00a7d3c4352fd4a7a1cf5fcfc277d6bf82415ea
gregor-J8 <gregor.johnson@outlook.com>
2025-05-20 12:45:52 -0500

    added endpoints for logs

--- Models/Log.cs
+++ Models/Log.cs
@@ -6,6 +6,8 @@ public class Log
     public string Title { get; set; }
     public string Comment { get; set; }
     public int UserId { get; set; }
+    public User? User { get; set; }
     public int CityId { get; set; }
+    public Cities? City { get; set; }
     public DateTime CreatedAt { get; set; }
 }
